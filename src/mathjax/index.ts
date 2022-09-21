f84de47aae8032b0f4e047906f6501a8c7bc8f6d
winstxnhdw <winstxnhdw@gmail.com>
2022-09-21 08:15:00 +0800

    works

--- src/mathjax/index.ts
+++ src/mathjax/index.ts
@@ -1,10 +1,13 @@
-import { mathjax } from 'mathjax-full/ts/mathjax'
-import { TeX } from 'mathjax-full/ts/input/tex'
-import { SVG } from 'mathjax-full/ts/output/svg'
-import { liteAdaptor } from 'mathjax-full/ts/adaptors/liteAdaptor'
-import { AllPackages } from 'mathjax-full/ts/input/tex/AllPackages'
+import { mathjax } from 'mathjax-full/js/mathjax'
+import { TeX } from 'mathjax-full/js/input/tex'
+import { SVG } from 'mathjax-full/js/output/svg'
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages'
+import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor'
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html'
 
 const adaptor = liteAdaptor()
+RegisterHTMLHandler(adaptor)
+
 const tex = new TeX({ packages: AllPackages.join(', ').split(/\s*,\s*/) })
 const svg = new SVG({ fontCache: 'local' })
 const mathjax_document = mathjax.document('', { InputJax: tex, OutputJax: svg })
