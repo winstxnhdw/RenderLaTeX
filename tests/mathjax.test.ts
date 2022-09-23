4e4d0be07b38d2c0a7269999f0967a3489cf349d
winstxnhdw <winstxnhdw@gmail.com>
2022-09-23 21:20:09 +0800

    move over to swc-node

--- tests/mathjax.test.ts
+++ tests/mathjax.test.ts
@@ -2,5 +2,6 @@ import mathjax_to_svg from '@/libs/mathjax'
 import { expect } from 'chai'
 
 describe('mathjax_to_svg', () => {
-  it('It should return a valid SVG', () => expect(mathjax_to_svg('x').to_string()).to.match(/^<svg/))
+  it('It should return a valid SVG when given a valid string', () =>
+    expect(mathjax_to_svg('x').to_string()).to.match(/^<svg/))
 })
