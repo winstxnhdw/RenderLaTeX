e1060a620755932db69cee9726d8d3879fa76986
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-09-26 02:32:56 +0800

    clean

--- scripts/get_bot_oauth.py
+++ scripts/get_bot_oauth.py
@@ -8,17 +8,19 @@ def main():
 
     load_dotenv()
     api_key = env.get("TWITTER_API_KEY")
+    str_input = lambda prompt: str(input(prompt))
     
     developer_url = f"https://twitter.com/oauth/request_token?oauth_consumer_key={api_key}&oauth_callback=oob"
-    print(f"Log into your developer account, and visit this URL:\n{developer_url}")
+    print(f"[!] Login to your *DEVELOPER* account and visit this URL:\n{developer_url}")
 
-    oauth_token = input("Enter the OAuth token: ")
+    oauth_token = str_input("[?] Enter the OAuth token: ")
     bot_url = f"https://twitter.com/oauth/authenticate?oauth_token={oauth_token}"
-    print(f"Now login to your bot account, and visit this URL:\n{bot_url}")
+    print(f"[!] Login to your *BOT* account and visit this URL:\n{bot_url}")
 
-    pin = input("Enter the PIN: ")
+    pin = str_input("[?] Enter the PIN: ")
     response = post(f"https://twitter.com/oauth/access_token?oauth_token={oauth_token}&oauth_verifier={pin}")
     
+    print("[*] Your OAuth tokens are:")
     [print(token) for token in response.text.split("&")[0:2]]
 
 if __name__ == '__main__':
