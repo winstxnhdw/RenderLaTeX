590ecf0c33694d16751daf4a73be3b9266499879
winstxnhdw <winstxnhdw@gmail.com>
2022-10-01 06:16:37 +0800

    Better test response

--- tests/filter.test.ts
+++ tests/filter.test.ts
@@ -2,6 +2,6 @@ import { validate_mentions } from '@/bot/filters'
 import { expect } from 'chai'
 
 describe('validate_mentions', () => {
-  it('It should return true', () => expect(validate_mentions('@bot render')).to.be.true)
-  it('It should return false', () => expect(validate_mentions('RT @bot test')).to.be.false)
+  it('It should return true if tweet is a mention', () => expect(validate_mentions('@bot render')).to.be.true)
+  it('It should return false if the tweet is a retweet', () => expect(validate_mentions('RT @bot test')).to.be.false)
 })
