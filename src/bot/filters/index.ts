09f0a70defebe8f02db1ef08a11194d9ceefb11b
winstxnhdw <winstxnhdw@gmail.com>
2022-10-01 06:17:42 +0800

    add return types

--- src/bot/filters/index.ts
+++ src/bot/filters/index.ts
@@ -2,9 +2,9 @@ import { illegal_words } from '@/bot/filters/banned_words'
 import { commands } from '@/bot/commands'
 import { FilterBuilder } from '@/bot/filters/FilterBuilder'
 
-const ignore_retweets = (tweet: string) => !tweet.startsWith('RT')
+const ignore_retweets = (tweet: string): Boolean => !tweet.startsWith('RT')
 
-const ignore_tweets_with_illegal_words = (tweet: string) => {
+const ignore_tweets_with_illegal_words = (tweet: string): Boolean => {
   for (const word of illegal_words) {
     if (!tweet.includes(word)) continue
     return false
@@ -13,16 +13,16 @@ const ignore_tweets_with_illegal_words = (tweet: string) => {
   return true
 }
 
-const ignore_tweets_without_commands = (tweet: string) => {
+const ignore_tweets_without_commands = (tweet: string): Boolean => {
   for (const command of Object.values(commands)) {
-    if (!tweet.slice(tweet.indexOf(' ') + 1).startsWith(command)) continue
+    if (!tweet.slice(tweet.indexOf(' ') + 1).startsWith(command.name)) continue
     return true
   }
 
   return false
 }
 
-export const validate_mentions = (tweet: string) => {
+export const validate_mentions = (tweet: string): Boolean => {
   const filter_builder = new FilterBuilder<string>()
   filter_builder.add(ignore_tweets_with_illegal_words)
   filter_builder.add(ignore_retweets)
