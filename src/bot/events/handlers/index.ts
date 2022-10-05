6c8242768cfc790a81942ab61821850f5293080a
winstxnhdw <winstxnhdw@gmail.com>
2022-10-06 02:32:41 +0800

    use full_text if possible

--- src/bot/events/handlers/index.ts
+++ src/bot/events/handlers/index.ts
@@ -4,11 +4,12 @@ import { get_command_input } from '@/bot/commands/helpers'
 
 export const handle_mentions = async (tweet_id: string) => {
   const tweet = await twitter_client.get_tweet(tweet_id)
+  const text = tweet.full_text || tweet.text
 
   for (const command of Object.values(commands)) {
-    if (!tweet.text.slice(tweet.text.indexOf(' ') + 1).startsWith(command.name)) continue
+    if (!text.slice(text.indexOf(' ') + 1).startsWith(command.name)) continue
 
-    await command.execute(twitter_client, { input: get_command_input(tweet.text, command.name), ...tweet })
+    await command.execute(twitter_client, { input: get_command_input(text, command.name), ...tweet })
     return
   }
 }
