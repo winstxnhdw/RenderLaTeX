30c4b859a434f7ebfa4eff69781d1e80db916f31
winstxnhdw <winstxnhdw@gmail.com>
2022-10-06 01:24:01 +0800

    log before and after

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -8,6 +8,8 @@ type TweetWithInput = TweetV2 & {
 }
 
 const render = async (twitter_client: TwitterClient, tweet: TweetWithInput) => {
+  console.log(tweet)
+
   if (tweet.input) await reply_with_latex(twitter_client, tweet.input, tweet.id)
 
   if (tweet.public_metrics?.reply_count !== 0) {
