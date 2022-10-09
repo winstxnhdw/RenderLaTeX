628a4b64aa3e3458f7b5ca67f48c706362369bfe
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-10-09 18:40:14 +0800

    Update index.ts

--- src/bot/events/handlers/index.ts
+++ src/bot/events/handlers/index.ts
@@ -5,6 +5,7 @@ import { get_command_input } from '@/bot/commands/helpers'
 export const handle_mentions = async (tweet_id: string) => {
   const tweet = await twitter_client.get_tweet(tweet_id)
   const text = tweet.full_text || tweet.text
+  console.log(text)
 
   for (const command of Object.values(commands)) {
     if (!text.slice(text.indexOf(' ') + 1).startsWith(command.name)) continue
