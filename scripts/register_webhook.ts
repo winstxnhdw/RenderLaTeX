9df021ceebe627b2920be30f01f68e006fb85144
winstxnhdw <winstxnhdw@gmail.com>
2022-10-31 23:41:03 +0800

    style(scripts): use path aliases

--- scripts/register_webhook.ts
+++ scripts/register_webhook.ts
@@ -1,5 +1,5 @@
-import { config } from '../src/config'
-import { twitter_activity } from '../src/libs/Twitter'
+import { config } from '@/config'
+import { twitter_activity } from '@/libs/Twitter'
 
 async function main() {
   twitter_activity.register_webhook(config.WEBHOOK_ENDPOINT)
