44597f651c84d92cc6d51eec3623bad91fd57e69
winstxnhdw <winstxnhdw@gmail.com>
2022-11-22 06:58:51 +0800

    fix: detect and replace special html characters

--- src/bot/features/reply_with_latex.ts
+++ src/bot/features/reply_with_latex.ts
@@ -4,9 +4,13 @@ import type { TwitterClient } from '@/libs/Twitter/TwitterClient'
 
 const reply_texts = ['#LaTeX', '#Mathematics', '#Math']
 
+const replace_html_special_chars = (tweet: string): string =>
+  tweet.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+
 export const reply_with_latex = async (twitter_client: TwitterClient, tweet: string, in_reply_to_status_id: string) => {
   const reply_text = get_random_element(reply_texts)
-  const webp_buffer = render_webp_latex(tweet)
+  const clean_tweet = replace_html_special_chars(tweet)
+  const webp_buffer = render_webp_latex(clean_tweet)
 
   await twitter_client.reply_with_media(reply_text, in_reply_to_status_id, webp_buffer, {
     mimeType: 'image/webp'
