794866f9593f3178296ea36d622f54c85e35d467
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-11-28 18:46:29 +0800

    refactor(webhook): use await on register_webhook

--- scripts/register_webhook.ts
+++ scripts/register_webhook.ts
@@ -2,7 +2,7 @@ import { config } from '@/config'
 import { twitter_activity } from '@/libs/Twitter'
 
 async function main() {
-  twitter_activity.register_webhook(config.WEBHOOK_ENDPOINT)
+  await twitter_activity.register_webhook(config.WEBHOOK_ENDPOINT)
 }
 
 main()
