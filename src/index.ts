6f047653fd4ffe0899c5217bcb5c499899d8acc8
winstxnhdw <winstxnhdw@gmail.com>
2023-02-01 22:36:05 +0800

    chore: use builtin aws types

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,17 @@
 import { listen_to } from '@/bot'
 import { twitter_activity } from '@/libs/Twitter'
-import type { Handler, APIGatewayProxyResultV2 } from 'aws-lambda'
-
-type LambdaFunctionURLEvent = {
-  version: string
-  routeKey: string
-  rawPath: string
-  rawQueryString: string
-  headers: {
-    host: string
-  }
-  queryStringParameters?: {
-    [key: string]: unknown
-  }
-  requestContext: {
-    accountId: string
-    apiId: string
-    domainName: string
-    domainPrefix: string
-    http: {
-      method: 'GET' | 'POST'
-      path: string
-      protocol: string
-      sourceIp: string
-      userAgent: string | null
-    }
-    requestId: string
-    routeKey: string
-    stage: string
-    time: string
-    timeEpoch: number
-  }
-  body?: string
-  isBase64Encoded: false
-}
+import type {
+  Handler,
+  APIGatewayProxyResultV2,
+  APIGatewayProxyEventV2WithRequestContext,
+  APIGatewayEventRequestContextV2
+} from 'aws-lambda'
 
 listen_to.tweet_create_events()
 
-export const handler: Handler = async (event: LambdaFunctionURLEvent): Promise<APIGatewayProxyResultV2> => {
+export const handler: Handler = async (
+  event: APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>
+): Promise<APIGatewayProxyResultV2> => {
   if (event.requestContext.http.method === 'GET') {
     const response = twitter_activity.handle_crc(event.queryStringParameters?.['crc_token'] as string)
     return typeof response === 'string'
