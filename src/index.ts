ff10a8874a72e2d755de02fff5b56b43aef25525
winstxnhdw <winstxnhdw@gmail.com>
2023-02-01 22:40:30 +0800

    chore: check if query is undefined

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,16 @@ listen_to.tweet_create_events()
 export const handler: Handler = async (
   event: APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>
 ): Promise<APIGatewayProxyResultV2> => {
-  if (event.requestContext.http.method === 'GET') {
-    const response = twitter_activity.handle_crc(event.queryStringParameters?.['crc_token'] as string)
-    return typeof response === 'string'
-      ? { statusCode: 400, body: response }
-      : { statusCode: 200, body: JSON.stringify(response) }
+  if (event.requestContext.http.method === 'POST') {
+    return (await twitter_activity.handle_post(event.body))
+      ? { statusCode: 200, body: 'OK!' }
+      : { statusCode: 400, body: 'Invalid request!' }
   }
 
-  return (await twitter_activity.handle_post(event.body))
-    ? { statusCode: 200, body: 'OK!' }
-    : { statusCode: 400, body: 'Invalid request!' }
+  if (event.queryStringParameters === undefined) return { statusCode: 400, body: 'Invalid request!' }
+  const response = twitter_activity.handle_crc(event.queryStringParameters['crc_token'] as string)
+
+  return typeof response === 'string'
+    ? { statusCode: 400, body: response }
+    : { statusCode: 200, body: JSON.stringify(response) }
 }
