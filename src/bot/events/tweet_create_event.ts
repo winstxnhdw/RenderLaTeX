fbc9aa54340f04e725a8b3467f13d856b05c048f
winstxnhdw <winstxnhdw@gmail.com>
2023-04-05 11:10:25 +0800

    perf: use `Promise.all` to not block async tasks

--- src/bot/events/tweet_create_event.ts
+++ src/bot/events/tweet_create_event.ts
@@ -1,10 +1,12 @@
+import { handle_mentions } from '@/bot/events/handlers'
 import { validate_mentions } from '@/bot/filters'
-import { handle_mentions } from './handlers'
 import type { TweetCreateEvents } from 'webhoot'
 
 export const handle_tweet_create_events = async (event: TweetCreateEvents) => {
-  for (const tweet_create_event of event.tweet_create_events) {
-    if (!validate_mentions(tweet_create_event.text)) continue
-    await handle_mentions(tweet_create_event.id_str)
-  }
+  await Promise.all(
+    event.tweet_create_events.map(async (tweet_create_event) => {
+      if (!validate_mentions(tweet_create_event.text)) return
+      await handle_mentions(tweet_create_event.id_str)
+    })
+  )
 }
